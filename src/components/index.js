ce46441cc66bca4298c51f4bd13e9a2d76f19996
Tom Duffy <thomas.edward.duffy@gmail.com>
2020-10-09 16:06:48 -0700

    fixed particle opacity

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,8 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     particlesCanvas: {
-        position: "absolute"
+        position: "absolute",
+        opacity: 0.2
     }
 });
 
