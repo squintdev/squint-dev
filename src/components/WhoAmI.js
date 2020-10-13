c905d6e8ec9e02e77aeaa2d8bed5690da4271cf6
Tom Duffy <thomas.edward.duffy@gmail.com>
2020-10-12 18:17:42 -0700

    created functional compenent for whoami

---
+++ src/components/WhoAmI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhoAmI = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WhoAmI;
--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,7 @@ import {makeStyles} from '@material-ui/core/styles';
 const useStyles = makeStyles({
     particlesCanvas: {
         position: "absolute",
-        opacity: 0.2
+        opacity: 0.3
     }
 });
 
