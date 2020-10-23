9758aa8d5fd2aebf130c6476eb4d129a0e38e4aa
Tom Duffy <thomas.edward.duffy@gmail.com>
2020-10-22 17:13:01 -0700

    added blog page to router

---
+++ src/components/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Blog = () => {
+    return (
+        <>
+            <Navbar />
+            <h2 className="soon">Coming Soon...</h2>
+        </>
+    );
+};
+
+export default Blog;
