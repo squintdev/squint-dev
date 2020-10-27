177463909d2783b8fa74855a6fadd5f069ed48af
Tom Duffy <thomas.edward.duffy@gmail.com>
2020-10-27 15:51:24 -0700

    api calls are working

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import GhostContentAPI from '@tryghost/content-api';
+import {makeStyles} from '@material-ui/core/styles';
+import {Typography, Box} from '@material-ui/core';
 import Navbar from './Navbar';
 
+const useStyles = makeStyles(theme=>({
+    mainContainer: {
+        background: "#011c39",
+        padding: "3rem"
+    },
+    multiBlogTitle: {
+        color: "#da4646",
+        padding: "3rem 0",
+        textTransform: "uppercase"
+    }
+}));
+
+const api = new GhostContentAPI({
+    url: 'https://squintghost.herokuapp.com',
+    key: '524e060a1d8a8b1df204be3fcf',
+    version: "v2"
+});
+
+//grab all posts
+api.posts
+    .browse({include: 'tags, authors'})
+    .then((posts) => {
+        posts.forEach((post) => {
+            console.log(post);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 const Blog = () => {
+    const classes = useStyles();
     return (
         <>
             <Navbar />
-            <h2 className="soon">Coming Soon...</h2>
+            <Box component="header" className={classes.mainContainer}>
+                <Typography variant="h3" className={classes.multiBlogTitle}>
+                    Test
+                </Typography>
+            </Box>
         </>
     );
 };
