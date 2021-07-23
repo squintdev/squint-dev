4d67bed0fd87f3fd46385de69613116e73715734
Tom Duffy <thomas.edward.duffy@gmail.com>
2021-07-23 14:07:02 -0700

    added ssw-gallery

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import WhoAmI from './components/WhoAmI';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import sdRgB from './components/sdRgB';
+import Gallery from './components/Gallery';
+import SSWGallery from './components/SSWGallery';
+import SOOLGallery from './components/SOOLGallery';
+import RGBGallery from './components/RGBGallery';
 
 const App = () => {
     return (
@@ -22,6 +26,10 @@ const App = () => {
             <Route path="/projects" component={Projects} />
             <Route path="/contact" component={Contact} />
             <Route path="/sdRgB" component={sdRgB} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/ssw-curation" component={SSWGallery} />
+            <Route path="/sool-gallery" component={SOOLGallery} />
+            <Route path="/rgb-gallery" component={RGBGallery} />
         </>
     );
 }
